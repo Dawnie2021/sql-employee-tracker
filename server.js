3ae03ef1c58a4dcad0158280484f6024d101abd3
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-01-17 15:30:16 -0500

    added a prompt system

--- server.js
+++ server.js
@@ -1,2 +1,31 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const prompt = inquirer.createPromptModule();
+
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'employee_db'
+    }
+);
+
+
+
+prompt([
+    {
+        type: 'list',
+        name: 'choices',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Exit'
+            
+        ]
+    }]);
