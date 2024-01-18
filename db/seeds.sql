8e94780fa99a3c46e9a28caebf4af104b997102f
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-01-17 22:28:20 -0500

    fixed some grammer issues

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,28 +1,30 @@
+USE staff_db; 
+
 INSERT INTO department(name) 
 VALUES
-(Development),
-(Product Management),
-(Quality Assurance),
-(Human Resources);
+('Development'),
+('Product Management'),
+('Quality Assurance'),
+('Human Resources');
 
 INSERT INTO role(title, salary, department_id)
 VALUES
-('software engineer', 86,000, 1),
-('front-end developer', 72,000, 1),
-('project manager', 101,000, 2),
-('business analyst', 93,000, 2),
-('QA Engineer', 81,000, 3),
-('HR manager' 69,000, 4),
+('software engineer', 86000, 1),
+('front-end developer', 72000, 1),
+('project manager', 101000, 2),
+('business analyst', 93000, 2),
+('QA Engineer', 81000, 3),
+('HR manager', 69000, 4);
 
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES
-('Katie', 'Wynn', 2, 4),
-('Jade', 'Black', 1, 4),
-('Amber', 'Smith', 6, NULL),
-('Carlina', 'Brown', 3, 3),
-('Gianna', 'Chung', 4, 3),
-('Joe', 'Chu', 5, NULL);
+('Katie', 'Wynn', 2, 1),
+('Jade', 'Black', 1, 1),
+('Amber', 'Smith', 6, 3),
+('Carlina', 'Brown', 3, 2),
+('Gianna', 'Chung', 4, 2),
+('Joe', 'Chu', 5, 4);
 
 
 
