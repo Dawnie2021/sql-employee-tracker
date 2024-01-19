7ba589865646c712988f6f556f43d0e679128b32
Dawn nguyen <dawnnguyen2021@gmail.com>
2024-01-19 05:28:06 -0500

    added more prompts

--- server.js
+++ server.js
@@ -16,54 +16,82 @@ const db = mysql.createConnection(
 
 const prompt = inquirer.createPromptModule();
 const init = () => {
-prompt(
-    {
-        type: 'list',
-        message: 'What would you like to do?',
-        name: 'view',
-        choices: [
-            'All departments',
-            'All roles',
-            'All employees',
-            // 'Add a department',
-            // 'Add a role',
-            // 'Add an employee',
-            // 'Update an employee role',
-            'Exit']
-    }).then((answer) => {
-       
+    prompt(
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'view',
+            choices: [
+                'All departments',
+                'All roles',
+                'All employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Exit']
+        }).then((answer) => {
+
             if (answer.view === 'All departments')
-        db.query('SELECT * FROM department', (error, departments) => {
-            if (error) console.error(error);
-            console.table(departments)
-            init(); 
-        });
-        if (answer.view === 'All roles')
-        db.query('SELECT * FROM role', (error, roles) => {
-            if (error) console.error(error);
-            console.table(roles)
-            init(); 
-        });
-        if (answer.view === 'All employees')
-        db.query('SELECT * FROM employee', (error, employees) => {
-            if (error) console.error(error);
-            console.table(employees)
-            init(); 
-        });
+                db.query('SELECT * FROM department', (error, departments) => {
+                    if (error) console.error(error);
+                    console.table(departments)
+                    init();
+                });
+            if (answer.view === 'All roles')
+                db.query('SELECT * FROM role', (error, roles) => {
+                    if (error) console.error(error);
+                    console.table(roles)
+                    init();
+                });
+            if (answer.view === 'All employees')
+                db.query('SELECT * FROM employee', (error, employees) => {
+                    if (error) console.error(error);
+                    console.table(employees)
+                    init();
+                });
 
-        Exit:() => {
-            process.exit();
+            if (answer.view === 'Add a department') {
+                inquirer.prompt({
+                    type: 'input',
+                    name: 'department',
+                    message: 'What is the name of the department?'
+                })
+            }
+
+            if (answer.view === 'Add a role') {
+                inquirer.prompt({
+                    type: 'input',
+                    name: 'role',
+                    message: 'What is the name of the role?',
+                
+
+
+                    type: 'input',
+                    name: 'salary',
+                    message: 'What is the salary?',
+
+                
+                })
+
+            }
+
+            if (answer.view === 'Add an employee') {
+                inquirer.prompt({
+                    type: 'input',
+                    name: 'employee',
+                    message: 'What is the name of the employee'
+                })
+            }
+
+            if (answer.view === 'Update an employee role')
         }
-        
-        
-    })
-        
-}
-       init(); 
+        )
+};
+init();
 
-        
-        
-       
-      
-  
-  
+
+
+
+
+
